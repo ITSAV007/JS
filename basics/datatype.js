9d7ab59b357201e794faaba8a4cfc6010cd93485
ITSAV007 <154081499+ITSAV007@users.noreply.github.com>
2023-12-17 18:19:35 +0000

    data type and variables done

---
+++ basics/datatype.js
@@ -0,0 +1,6 @@
+let name = "anil";
+let age = 34;
+let isLoggedIn = false;
+
+console.table({ name, age, isLoggedIn });
+console.log(typeof name);
---
+++ basics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 14455333;
+let accountEmail = "anil@gmail.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+
+accountEmail = "vishnoi@gmail.com";
+accountPassword = "4567";
+accountCity = "jodhpur";
+
+console.table([accountEmail, accountId, accountPassword,accountCity, accountState]);
